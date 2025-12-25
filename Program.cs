a5452a016c682f8ddd9c795a7c1493c52aa3857b
pnvnd <peter@datacrunch.ca>
2025-12-25 16:34:16 -0500

    add: ping and fib endpoints

--- Program.cs
+++ Program.cs
@@ -23,4 +23,31 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+// Health
+app.MapGet("/ping", () =>
+{
+    app.Logger.LogInformation("pong");
+    return Results.Json(new { message = "pong" });
+});
+
+// Fibonacci
+app.MapGet("/fib", (HttpRequest req) =>
+{
+    var x = req.Query["n"].ToString();
+    if (!int.TryParse(x, out var n) || n < 0 || n > 90)
+    {
+        app.Logger.LogWarning("{value} is not 0 <= n <= 90", x);
+        return Results.BadRequest(new { error = $"{x} is not 0 <= n <= 90" });
+    }
+
+    var array = new List<long> { 0L, 1L };
+    for (int i = 2; i <= n; i++)
+    {
+        array.Add(array[i - 1] + array[i - 2]);
+    }
+
+    app.Logger.LogInformation($"Fibonacci number {n} is {array[n]}.");
+    return Results.Json(new { fibonacci_index = n, fibonacci_number = array[n] });
+});
+
 app.Run();
